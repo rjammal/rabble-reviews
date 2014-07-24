b400757255ba02cf8b085a47290c74537940ddc9
Rosemary Jammal <rjammal@gmail.com>
2014-07-24 10:07:38 -0700

    fixed bug that was not allowing passwords to get sent to rails

--- app/assets/javascripts/models/session.js
+++ app/assets/javascripts/models/session.js
@@ -1,3 +1,10 @@
 RabbleReviews.Models.Session = Backbone.Model.extend({
-    url: "api/session"
+    url: "api/session",
+
+    toJSON: function () {
+        //add password to existing json
+        var json = Backbone.Model.prototype.toJSON.call(this);
+        json["password"] = this.get("password");
+        return {"session": json};
+    }
 });
--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,3 +1,11 @@
 RabbleReviews.Models.User = Backbone.Model.extend({
-    urlRoot: "api/users"
+    urlRoot: "api/users", 
+
+    toJSON: function () {
+        //add password to existing json
+        var json = Backbone.Model.prototype.toJSON.call(this);
+        json["password"] = this.get("password");
+        //debugger
+        return {"user": json};
+    }
 });

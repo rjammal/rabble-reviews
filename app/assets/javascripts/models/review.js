7a73b228b488d9e934636e1cc88bf125622e956f
Rosemary Jammal <rjammal@gmail.com>
2014-07-25 22:18:13 -0700

    made the reiew show page significantly nicer looking

--- app/assets/javascripts/models/review.js
+++ app/assets/javascripts/models/review.js
@@ -1,3 +1,16 @@
 RabbleReviews.Models.Review = Backbone.Model.extend({
-    urlRoot: "api/reviews"
+    urlRoot: "api/reviews", 
+
+    author: function () {
+        this._author = this._author || new RabbleReviews.Models.User();
+        return this._author;
+    }, 
+
+    parse: function (response) {
+        if (response.author) {
+            this._author = new RabbleReviews.Models.User(response.author);
+            delete response.author
+        }
+        return response;
+    }
 });

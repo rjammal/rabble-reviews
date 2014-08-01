8b291480c9e1a0deb70a9c73a1312a36e9d44ac8
Rosemary Jammal <rjammal@gmail.com>
2014-07-31 20:16:17 -0700

    fixed pug with adding new games

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -15,7 +15,6 @@ class Api::GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
-    puts game.image.url(:thumbnail)
 
     genres = params[:genres] || []
     # convert genre names to genre objects
